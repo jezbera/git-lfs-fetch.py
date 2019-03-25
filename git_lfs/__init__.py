7d468d5355b476153a3b49136be3c89fcf484d95
Miroslav Jezbera <m.jezbera@gmc.net>
2019-03-25 13:30:53 +0100

    * Support git alternate directory for LFS files * Support HTTP authentication * Various fixes

--- git_lfs/__init__.py
+++ git_lfs/__init__.py
@@ -2,13 +2,18 @@ from __future__ import division, print_function, unicode_literals
 
 import json
 import os
-import pprint
+import re
+import sys
+import base64
+from getpass import getpass
 from subprocess import CalledProcessError, check_output, PIPE, Popen, STDOUT
 try:
     from urllib.parse import urlsplit, urlunsplit
-    from urllib.request import Request, urlopen
+    from urllib import unquote
+    from urllib.request import Request, urlopen, HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener, install_opener
 except ImportError:
-    from urllib2 import Request, urlopen
+    from urllib2 import Request, urlopen, HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener, install_opener
+    from urllib2 import unquote
     from urlparse import urlsplit, urlunsplit
 
 from .utils import force_link, ignore_missing_file, in_dir, TempDir, TempFile
@@ -49,30 +54,7 @@ def get_lfs_endpoint_url(git_repo, checkout_dir):
             # SSH format: git@example.org:repo.git
             host, path = url_split.path.split('@', 1)[1].split(':', 1)
         url = urlunsplit(('https', host, path, '', ''))
-    del url_split
-
-    # need to get GHE auth token if available. issue cmd like this to get:
-    # ssh git@git-server.com git-lfs-authenticate foo/bar.git download
-    if path.endswith('/info/lfs'):
-        path = path[:-len('/info/lfs')]
-    auth_header = get_lfs_api_token(host, path)
-    return url, auth_header
-
-
-def get_lfs_api_token(host, path):
-    """ gets an authorization token to use to do further introspection on the
-        LFS info in the repository.   See documentation here for description of
-        the ssh command and response:
-        https://github.com/git-lfs/git-lfs/blob/master/docs/api/server-discovery.md
-    """
-    header_info = {}
-    query_cmd = 'ssh git@'+host+' git-lfs-authenticate '+path+' download'
-    output = check_output(query_cmd.split()).strip().decode('utf8')
-    if output:
-        query_resp = json.loads(output)
-        header_info = query_resp['header']
-
-    return header_info
+    return url
 
 
 def find_lfs_files(checkout_dir):
@@ -101,6 +83,8 @@ def find_lfs_files(checkout_dir):
             continue
         if path in paths:
             continue
+        if not os.path.isfile(path) or os.stat(path).st_size == 0:
+            continue
         paths.add(path)
         yield path.decode('ascii')
 
@@ -118,22 +102,39 @@ def read_lfs_metadata(checkout_dir):
         yield (path, oid, size)
 
 
-def fetch_urls(lfs_url, lfs_auth_info, oid_list):
+def fetch_urls(lfs_url, oid_list):
     """Fetch the URLs of the files from the Git LFS endpoint
     """
     data = json.dumps({'operation': 'download', 'objects': oid_list})
-    headers = dict(POST_HEADERS)
-    headers.update(lfs_auth_info)
-    req = Request(lfs_url+'/objects/batch', data.encode('ascii'), headers)
+    req = Request(lfs_url+'/objects/batch', data.encode('ascii'), POST_HEADERS)
     resp = json.loads(urlopen(req).read().decode('ascii'))
     assert 'objects' in resp, resp
     return resp['objects']
+
+def link(cached, dst):
+    """Replaces file in checkout directory from cache
+    """
+    st = os.stat(dst)
+    force_link(cached, dst)
+    os.chmod(dst, st.st_mode)
 
 
 def fetch(git_repo, checkout_dir=None, verbose=0):
     """Download all the files managed by Git LFS
     """
     git_dir = git_repo+'/.git' if os.path.isdir(git_repo+'/.git') else git_repo
+    alternate_repo = git_dir
+    if os.path.exists(git_dir+'/objects/info/alternates'):
+        with open(git_dir+'/objects/info/alternates') as alt:
+            path = alt.readline()
+            if path[-1] == '\n': # read with enter at the end of line
+                path = path[:-1]
+            if path[-5:].find(".git") == -1: # usualy end with .git/objects
+                path = path.split(".git")[0] + ".git"
+            if (os.path.isdir(path)):
+                alternate_repo = path
+                if verbose > 0:
+                    print("Alternate git repository set to '%s'" % alternate_repo)
     checkout_dir = checkout_dir or git_repo
     if checkout_dir == git_dir:
         print('Can\'t checkout into a bare repo, please provide a valid '
@@ -163,15 +164,18 @@ def fetch(git_repo, checkout_dir=None, verbose=0):
 
         # If we have the file in the cache, link to it
         with ignore_missing_file():
-            cached = get_cache_dir(git_dir, oid)+'/'+oid
+            cached = get_cache_dir(alternate_repo, oid)+'/'+oid
             if os.stat(cached).st_size == size:
-                force_link(cached, dst)
+                link(cached, dst)
                 if verbose > 0:
                     print('Linked', path, 'from the cache')
                 continue
 
-        oid_list.append(dict(oid=oid, size=size))
-        lfs_files[(oid, size)] = path
+        if not (oid, size) in lfs_files:
+            oid_list.append(dict(oid=oid, size=size))
+            lfs_files[(oid, size)] = [path]
+        else:
+            lfs_files[(oid, size)] += [path]
 
     if not found:
         print('This repository does not seem to use LFS.')
@@ -183,31 +187,52 @@ def fetch(git_repo, checkout_dir=None, verbose=0):
         return
 
     # Fetch the URLs of the files from the Git LFS endpoint
-    lfs_url, lfs_auth_info = get_lfs_endpoint_url(git_repo, checkout_dir)
-
-    if verbose > 0:
-        print('Fetching URLs from %s ...' % lfs_url)
+    lfs_url = get_lfs_endpoint_url(git_repo, checkout_dir)
+    urlSearch = re.search('https://([^:]+?)(?::(.*?))?@', lfs_url)
+    username = None
+    password = None
+    if urlSearch != None:
+        username = unquote(urlSearch.group(1)).decode('utf-8')
+        password = urlSearch.group(2)
+        lfs_url = lfs_url.replace(urlSearch.group(0), 'https://')
     if verbose > 1:
-        print('Authorization info for URL: %s' % lfs_auth_info)
-        print('oid_list: %s' % pprint.pformat(oid_list))
-    objects = fetch_urls(lfs_url, lfs_auth_info, oid_list)
+        print('Fetching URLs from %s...' % lfs_url)
+    auth_header = {}
+    try:
+        objects = fetch_urls(lfs_url, oid_list)
+    except IOError, e:
+        if not hasattr(e, 'code') or e.code != 401:
+            raise e
+        if verbose > 1:
+            print('Site is requesting authorization...')
+        if username == None:
+            sys.stdout.write('User: ')
+            sys.stdout.flush()
+            username = sys.stdin.readline().rstrip()
+        if password == None:
+            password = getpass(username + ' password: ')
+        base64string = base64.b64encode('%s:%s' % (username, password))
+        auth_header = { 'Authorization': 'Basic %s' % base64string }
+        POST_HEADERS.update(auth_header)
+        objects = fetch_urls(lfs_url, oid_list)
 
     # Download the files
-    tmp_dir = git_dir+'/lfs/tmp'
+    if verbose > 0:
+        print("%d files have to be downloaded." % len(objects))
+    tmp_dir = alternate_repo +'/lfs/tmp'
     if not os.path.exists(tmp_dir):
         os.makedirs(tmp_dir)
     for obj in objects:
         oid, size = (obj['oid'], obj['size'])
-        path = lfs_files[(oid, size)]
-        cache_dir = get_cache_dir(git_dir, oid)
+        paths = lfs_files[(oid, size)]
+        cache_dir = get_cache_dir(alternate_repo, oid)
 
         # Download into tmp_dir
         with TempFile(dir=tmp_dir) as f:
             url = obj['actions']['download']['href']
-            head = obj['actions']['download']['header']
             print('Downloading %s (%s bytes) from %s...' %
-                  (path, size, url if verbose > 0 else url[:40]))
-            h = urlopen(Request(url, headers=head))
+                  (paths, size, url[:40]))
+            h = urlopen(Request(url, None, auth_header))
             while True:
                 buf = h.read(10240)
                 if not buf:
@@ -224,5 +249,8 @@ def fetch(git_repo, checkout_dir=None, verbose=0):
             os.rename(f.name, dst1)
 
         # Copy into checkout_dir
-        dst2 = checkout_dir+'/'+path
-        force_link(dst1, dst2)
+        for path in paths:
+            dst2 = checkout_dir+'/'+path
+            link(dst1, dst2)
+            if verbose > 0:
+                print('Checkout file %s (oid=%s)' % (dst2, oid))
